dffa3d374b8dd959e23560a56821f06bb7ca9a0f
vazirarasulova <rasulovavazira202gmail.com>
2022-02-23 23:05:30 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var elForm = document.querySelector(".form");
+var elFormInput = document.querySelector(".form__input");
+var elFormSelect = document.querySelector(".form__select");
+var elFormResult = document.querySelector(".form__result");
+
+
+elForm.addEventListener("submit" , function(e){
+
+  e.preventDefault();
+
+  var elFormInputVal = Number(elFormInput.value);
+  var elFormSelectVal = Number(elFormSelect.value);
+
+  elFormResult.textContent = (elFormInputVal * elFormSelectVal);
+  console.log(elFormInputVal * elFormSelectVal);
+});
+
+
